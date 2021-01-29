c2de822f505c8ace7185c3f140e458910a31343d
A.Karwowski <akk42@zips.uakron.edu>
2021-01-29 14:41:11 -0500

    Add more libraries and load data

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_wine
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+wineData = load_wine()
+print(wineData)
