733c7293113d6377552641675852172ccf960f06
A.Karwowski <akk42@zips.uakron.edu>
2021-02-07 12:35:42 -0500

    Final push only missing plots and graphs
    Co-authored-by: Chayton Hamric <saxguy1999@gmail.com>

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from matplotlib import cm
 import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
+from scipy import stats
 
+#Load the data into an object
+breastData = load_breast_cancer()
 
-digitsData = load_digits()
-x = digitsData.data
-y = digitsData.target
+#Split up the data
+x = breastData.data
+y = breastData.target
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
-"""cmap = cm.get_cmap('gnuplot')
-scatter = scatter_matrix(x_train,c=y_train,marker="o",s=40,hist_kwds={'bins':15}, figsize=(9,9),cmap=cmap)"""
-neighbors = np.arange(1,15)
+#Split The data into training and testing arrays
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=.75, random_state=20)
+
+#
+neighbors = np.arange(1,425)
 train_acc = np.empty(len(neighbors))
 test_acc = np.empty(len(neighbors))
 
@@ -25,12 +29,33 @@ for i,k in enumerate(neighbors):
 
     train_acc[i] = knn.score(x_train, y_train)
     test_acc[i] = knn.score(x_test, y_test)
-    print(f"Test Accuracy {test_acc[i]}")
-    print(f"Train Accuracy {train_acc[i]}")
-    print("====----====----====----====----====")
-
+    
 plt.plot(neighbors,test_acc,label="Testing Dataset Accuracy")
 plt.legend()
 plt.xlabel("n_neighbors")
 plt.ylabel("Accuracy")
 plt.show()
+
+
+
+
+unique, counts = np.unique(breastData.target,return_counts=True)
+classification = dict(zip(breastData.target_names, counts))
+print(f'Features and Attributes (Total): {len(breastData.feature_names)}')
+print(f'Features and Attributes (Type): {breastData.feature_names}')
+print(f'Number of Classes: {unique}')
+print(f'Distribution of Classes: {classification}')
+print(f'Dataset Partition (Training): 75.5%')
+print(f'Dataset Partition (Testing): 24.5%')
+print(f'Distance Metric: {knn.effective_metric_}')
+print(f'Estimate Accuracy at 5 Nearest Neighbors: {round(test_acc[3],3)}')
+print(f'Estimate Accuracy at 15 Nearest Neighbors: {round(test_acc[13],3)}')
+print(f'Estimate Accuracy at 30 Nearest Neighbors: {round(test_acc[28],3)}')
+print(f'Estimate Accuracy at 50 Nearest Neighbors: {round(test_acc[48],3)}')
+print(f'Min: {round(test_acc.min(),3)}')
+print(f'Max: {round(test_acc.max(),3)}')
+print(f'Mean: {round(test_acc.mean(),3)}')
+print(f'Median: {round(np.median(test_acc),3)}')
+print(f'Mode: {round(float(stats.mode(test_acc)[0]),3)}')
+print(f'STD: {round(np.std(test_acc),3)}')
+
